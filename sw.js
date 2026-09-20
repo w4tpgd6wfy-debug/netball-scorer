009917b4a01ed413e26f9b0e5c36ac8d6c3534f6
w4tpgd6wfy-debug <w4tpgd6wfy@privaterelay.appleid.com>
2026-09-20 09:19:16 +0800

    Refresh app cache for courtside update

--- sw.js
+++ sw.js
@@ -1,35 +1,26 @@
-const CACHE_NAME = "netball-scorer-v1";
-
-const FILES_TO_CACHE = [
-    "./",
-    "./index.html",
-    "./manifest.json"
-];
+const CACHE_NAME = "netball-scorer-v2";
+const FILES_TO_CACHE = ["./","./index.html","./manifest.json"];
 
 self.addEventListener("install", event => {
     event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            return cache.addAll(FILES_TO_CACHE);
-        })
+        caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
     );
+    self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
     event.waitUntil(
-        caches.keys().then(keys => {
-            return Promise.all(
-                keys
-                    .filter(key => key !== CACHE_NAME)
-                    .map(key => caches.delete(key))
-            );
-        })
+        caches.keys().then(keys =>
+            Promise.all(
+                keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+            )
+        )
     );
+    self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
     event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
+        caches.match(event.request).then(response => response || fetch(event.request))
     );
 });
