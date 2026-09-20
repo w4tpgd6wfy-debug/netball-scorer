434b7b2c81e0a27c475eb2b33c5fcddbd53d592a
w4tpgd6wfy-debug <w4tpgd6wfy@privaterelay.appleid.com>
2026-09-20 09:25:57 +0800

    Force refresh to latest app version

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "netball-scorer-v2";
+const CACHE_NAME = "netball-scorer-v3";
 const FILES_TO_CACHE = ["./","./index.html","./manifest.json"];
 
 self.addEventListener("install", event => {
